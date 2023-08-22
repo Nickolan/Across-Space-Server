88913110339527afd24f8da15632279caedea9a8
Nicolas <nicolassantiagonavarrete.nsn@gmail.com>
2023-08-22 12:51:48 -0300

    dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,17 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { PlanetsModule } from './planets/planets.module';
 import { SatellitesModule } from './satellites/satellites.module';
 import { AuthModule } from './auth/auth.module';
+require('dotenv').config();
+const { DB_USER, DB_PASSWORD, DB_NAME, DB_DEPLOY, DB_HOST} = process.env
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'mysql',
-    host: 'localhost',
+    host: DB_HOST,
     port: 3306,
-    username: 'root',
-    password: 'mysqlnikolan',
-    database: 'spacezdb',
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
     synchronize: true
   }),
